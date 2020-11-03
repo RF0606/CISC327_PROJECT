b74ecef159f4e361924b163af36cc78481f9bad0
ytc0211 <70901697+ytc0211@users.noreply.github.com>
2020-11-03 10:12:35 +0800

    Revert "add R4, R5 and R6"

--- qa327/app.py
+++ qa327/app.py
@@ -1,14 +1,8 @@
 import csv
 import sys
 import os
-import time
 
-status = True
-user_name = 'test_name'
-user_email = 'test@test.com'
-user_password = '123asd'
-balance = 100
-
+status = False
 userFile = open('user.csv', 'a+')
 ticketFile = open('ticket.csv', 'a+')
 tranFile = open('transaction.csv', 'a+')
@@ -49,6 +43,7 @@ def main():
 
 
 def R1():
+    print('landing')
     if status:
         print('Your Balance:', balance)
         input1 = input('Type your choice:\nsell  buy  update  logout\n')
@@ -85,57 +80,15 @@ def R3():
 
 
 def R4():
-    print('Selling session started successfully!')
-    try:
-        ticket_name, price, quantity, date = input('Please type ticket name, price, quantity, date:').split(',')
-    except:
-        print('Please retype!\nThe number of inputs should be 4.')
-        R4()
-    if not (check_ticket_name(ticket_name) and check_price(price) and check_quantity_sell(quantity) and check_date(
-            date)):
-        R4()
-    price = eval(price)
-    price = round(price, 2)
-    tranWriter.writerow(['sell', user_name, ticket_name, price, quantity, date])
-    print('Selling transaction was created successfully.')
-    R1()
+    print('sell')
 
 
 def R5():
-    print('Buying session started successfully!')
-    try:
-        ticket_name, quantity = input('Please type ticket name, quantity:').split(',')
-    except:
-        print('Please retype!\nThe number of inputs should be 2.')
-        R5()
-    for i in ticketReader:
-        if ticket_name == ticketReader[i][0]:
-            price = ticketReader[i][1]
-            aval_quantity = ticketReader[i][2]
-    if not(check_ticket_name(ticket_name) and check_quantity_buy(price, quantity, aval_quantity)):
-        R5()
-    price = eval(price)
-    price = round(price, 2)
-    tranWriter.writerow(['sell', user_name, ticket_name, price, quantity])
-    print('Buying transaction was created successfully.')
-    R1()
+    print('buy')
 
 
 def R6():
-    print('Updating session started successfully!')
-    try:
-        ticket_name, price, quantity, date = input('Please type ticket name, price, quantity, date:').split(',')
-    except:
-        print('Please retype!\nThe number of inputs should be 4.')
-        R6()
-    if not (check_ticket_name(ticket_name) and check_price(price) and check_quantity_sell(quantity) and check_date(
-            date)):
-        R6()
-    price = eval(price)
-    price = round(price, 2)
-    tranWriter.writerow(['sell', user_name, ticket_name, price, quantity])
-    print('Updating transaction was created successfully.')
-    R1()
+    print('update')
 
 
 def R7():
@@ -143,75 +96,8 @@ def R7():
 
 
 def R8():
+    print('exit')
     userFile.close()
     ticketFile.close()
     tranFile.close()
     exit(0)
-
-
-def check_ticket_name(ticket_name):
-    if not (ticket_name.isalnum() or ticket_name.isspace()):
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nTicket name should be '
-              'alphanumeric-only.')
-        return False
-    if not (ticket_name[0].isspace() or ticket_name[len(ticket_name) - 1].isspace):
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nSpace allowed only if it is not the '
-              'first or the last character.')
-        return False
-    elif len(ticket_name) > 60:
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nThe ticket name should be no longer '
-              'than 60 characters.')
-        return False
-    return True
-
-
-def check_price(price):
-    if not (price.isdigit()):
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nThe ticket price should be numeric.')
-        return False
-    price = eval(price)
-    if not (10 <= price <= 100):
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nThe ticket price should be of range ['
-              '10, 100].')
-        return False
-    return True
-
-
-def check_quantity_sell(quantity):
-    quantity = eval(quantity)
-    if not (isinstance(quantity, int)):
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nThe ticket quantity should be an '
-              'integer.')
-        return False
-    if not (0 < quantity <= 100):
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nThe quantity of the tickets has to be '
-              'more than 0, and less than or equal to 100.')
-        return False
-    return True
-
-
-def check_date(date):
-    try:
-        time.strptime(date, "%Y%m%d")
-        return True
-    except:
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nDate must be given in the format '
-              'YYYYMMDD.')
-        return False
-
-
-def check_quantity_buy(price, quantity, aval_quantity):
-    quantity = eval(quantity)
-    aval_quantity = eval(aval_quantity)
-    if not (isinstance(quantity, int)):
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nThe ticket quantity should be an '
-              'integer.')
-        return False
-    if not (0 < quantity <= aval_quantity):
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nThe quantity of the tickets has to be '
-              'more than 0, and less than or equal to the available quantity.')
-        return False
-    elif not (balance >= price * quantity * 1.35 *1.05):
-        print('Selling transaction was created unsuccessfully.\nPlease retype!\nYour balance is insufficient.')
-        return False
-    return True
