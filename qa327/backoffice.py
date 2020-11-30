268008da96f4f6f7b4a26021696f9bec9a82c075
peterFang <15rf17@queensu.ca>
2020-11-30 13:38:58 -0500

    add buying into backoffice

--- qa327/backoffice.py
+++ qa327/backoffice.py
@@ -31,7 +31,43 @@ def sell_process():
 
 
 def buy_process():
-    pass
+    for i in tranReader:
+        if not i:
+            continue
+        if i[0] == 'buying':
+            user_email = i[1]
+            ticket_name = i[2]
+            price = eval(i[3])
+            buyquantity = eval(i[4])
+            owneremail = ''
+            for j in ticketReader: # look for the ticket of this owner
+                if not j:
+                    continue
+                if ticket_name == j[0]:
+                    quantity = eval(j[2])
+                    if buyquantity > quantity:
+                        print('Maximum purchase quantity exceeded')
+                        continue
+                    if buyquantity <= quantity:
+                        totalquantity = quantity - buyquantity
+                        j[2] = totalquantity
+                        owneremail = j[3]
+            for j in accReader:
+                if not j:
+                    continue
+                if user_email == j[0]:
+                    balance = eval(j[3])
+                    total_price = price * buyquantity * 1.35 * 1.05
+                    balance = balance - total_price
+                    j[3] = balance
+            for j in accReader:
+                if not j:
+                    continue
+                if owneremail == j[0]:
+                    balance = eval(j[3])
+                    total_earning = price * buyquantity
+                    balance = balance + total_earning
+                    j[3] = balance
 
 
 def update_process():
@@ -44,6 +80,8 @@ def update_process():
             price = i[3]
             quantity = i[4]
             for j in ticketReader:          # look for the ticket of this owner
+                if not j:
+                    continue
                 if ticket_name == j[0] and user_email == j[3]:
                     j[1] = price            # modify the info
                     j[2] = quantity
