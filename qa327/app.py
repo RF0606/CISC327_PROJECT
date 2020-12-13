0d06a3020f72e15c7aeae495799f39762c0148eb
YTC <ytc214800722@gmail.com>
2020-12-14 03:08:13 +0800

    fix bugs in app.py

--- qa327/app.py
+++ qa327/app.py
@@ -8,12 +8,13 @@ user_email = ''
 user_password = ''
 balance = -1
 
-accFile = open('accounts.csv', 'r')
-ticketFile = open('tickets.csv', 'r')
-tranFile = open('transaction.csv', 'a+')
+accFile = open('accounts.csv', 'r+')
+ticketFile = open('tickets.csv', 'r+')
 accReader = csv.reader(accFile)
 ticketReader = csv.reader(ticketFile)
-tranWriter = csv.writer(tranFile)
+
+location_arg = open('backend_locations.txt', 'r').readline()
+tranWriter = list(csv.reader(open(location_arg[0]+'_transactions.csv')))
 
 
 def main():
@@ -87,6 +88,8 @@ def R3():
     if not (check_register_email(login_email) and check_register_password(login_password)):
         R1()  # check the format of inputs. return R1 if there is anything invalid
     for i in accReader:  # go over every user info to check login
+        if not i:
+            continue
         if login_email == i[0] and login_password == i[2]:
             global status, user_name, user_email, user_password, balance
             # set global value to be the user info if login succeeded
@@ -133,6 +136,8 @@ def R5():
         R1()    # check the format of inputs. return R1 if there is anything invalid
     count = 0
     for i in ticketReader:  # go over every ticket to check if exists
+        if not i:
+            continue
         if ticket_name == i[0]:
             price = i[1]
             aval_quantity = i[2]
@@ -163,6 +168,8 @@ def R6():
         R1()    # check the format of inputs. return R1 if there is anything invalid
     count = 0
     for i in ticketReader:  # go over every ticket to check if exists
+        if not i:
+            continue
         if ticket_name == i[0] and user_email == i[3]:
             count += 1
     if count == 0:
@@ -317,7 +324,7 @@ def check_exits_email(register_email):
     accReader = csv.reader(open('accounts.csv', 'r'))  # read the file
     # if input email already exits, return False
     for i in accReader:
-        if i == '':
+        if not i:
             continue
         elif register_email == i[0]:
             print("account exits\n")
